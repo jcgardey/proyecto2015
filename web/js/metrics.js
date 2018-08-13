9169db18d4585afb914433417feb04c8c47a44b6
Juan Cruz Gardey <juancruzgardey@gmail.com>
2018-08-13 15:28:56 -0300

    updated HoverTime metric

--- web/js/metrics.js
+++ web/js/metrics.js
@@ -1,38 +1,61 @@
-var wrapperOffset = 30;
+var wrapperOffset = 20;
 function wrapElement(element) {
 	var rectangle = element.getBoundingClientRect();
-	var canvas = document.createElement('canvas');			
-	canvas.style.position='absolute';
-	canvas.style.zIndex=100000;
-	canvas.style.left = rectangle.left - wrapperOffset + "px";
-	canvas.style.right = rectangle.right + wrapperOffset + "px";
-	canvas.style.top = rectangle.top - wrapperOffset + "px";
-	canvas.style.bottom = rectangle.bottom + wrapperOffset + "px";
+	var wrapper = document.createElement('div');			
+	wrapper.style.position='absolute';
+	wrapper.style.zIndex= 999;
+	wrapper.style.left = rectangle.left - wrapperOffset + "px";
+	wrapper.style.right = rectangle.right + wrapperOffset + "px";
+	wrapper.style.top = rectangle.top - wrapperOffset + "px";
+	wrapper.style.bottom = rectangle.bottom + wrapperOffset + "px";
 
-	canvas.width = (rectangle.right + wrapperOffset) - (rectangle.left - wrapperOffset);
-	canvas.height = (rectangle.bottom + wrapperOffset) - (rectangle.top - wrapperOffset);
-	element.parentElement.insertBefore(canvas, element);
-	$(canvas).attr('data-type', element.tagName.toLowerCase());
-	canvas.id = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 7);
-	return canvas;
+	wrapper.style.width = (rectangle.right + wrapperOffset) - (rectangle.left - wrapperOffset) + "px";
+	wrapper.style.height = (rectangle.bottom + wrapperOffset) - (rectangle.top - wrapperOffset) + "px";
+	element.parentElement.insertBefore(wrapper, element);
+	
+	$(wrapper).attr('data-type', element.tagName.toLowerCase());
+	wrapper.id = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 7);
+
+	element.style.position = 'relative';
+	element.style.zIndex = 99999;
+	
+	return wrapper;
 }
 
 function wrapElements() {
-	$("a, input, select").each(function(i, element) {
-		console.log(element);
+	$("a, input[type='text'], input[type='password'], select").each(function(i, element) {
 		wrapElement(element);
 	});
 }
 
 function HoverTime() {
-	var mouseOverTime = 0;
-	$("canvas[data-type='input'], canvas[data-type='a'], canvas[data-type='select']").on("mouseover", function(e) {
-		mouseOverTime = e.timeStamp;
-	});
-	$("canvas[data-type='input'], canvas[data-type='a'], canvas[data-type='select']").on("mouseout", function(e) {
+	var lastMouseMove = 0;
+	var lastElement = null;
+	var threshold = 1000;
+	var inactiveTime = 0;
+	var moveTime = 0;
+	$("div[data-type='input'], div[data-type='a'], div[data-type='select'],a, input[type='text'], input[type='password'], select").on("mousemove", function (e) {	
 		var now = e.timeStamp;
-		console.log("Hover time " + (now - mouseOverTime));
-		mouseOverTime = now;
+		console.log("cantidad " + (now - lastMouseMove));
+		if ((now - lastMouseMove) >= threshold) {
+			inactiveTime += (now - lastMouseMove);
+		}
+		else {
+			moveTime += (now - lastMouseMove);
+		}
+		lastMouseMove = now;
+	});
+
+	$("div[data-type='input'], div[data-type='a'], div[data-type='select']").on("mouseleave", function(e) {
+
+		// estoy pasando del wrapper al elemento, por lo tanto no se debe resetear el contador
+		if (e.relatedTarget == e.target.nextSibling) {
+			return null;
+		} 
+		console.log("Mouse move time " + moveTime );
+		console.log("Mouse inactive time " + inactiveTime);
+		moveTime = 0;
+		inactiveTime = 0;
 	});
 
 
